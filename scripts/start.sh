eda5a8410a6274e9213a9287a8c2ded618a25f0e
Samuel Woodbridge <s-bridge@live.co.uk>
2017-11-18 18:45:20 +0000

    [Scripts] - Start script changes for codedeploy

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nohup java -jar /home/ubuntu/finatra/target/scala-2.12/api-finatra-assembly-1.0.0.jar -http.port=":80" &
+java -jar /home/ubuntu/finatra/target/scala-2.12/api-finatra-assembly-1.0.0.jar -http.port=":80" > /dev/null 2>&1 &
