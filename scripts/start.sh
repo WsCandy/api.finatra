fb83b61977a310d8c7f239e4efe29e95e16dcf07
Samuel Woodbridge <s-bridge@live.co.uk>
2017-11-18 00:07:28 +0000

    [AppSpec] - More changes

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo nohup java -Dname="api.finatra" -jar finatra/target/scala-2.12/api-finatra-assembly-1.0.0.jar -http.port=":80" &
+nohup java -Dname="api.finatra" -jar finatra/target/scala-2.12/api-finatra-assembly-1.0.0.jar -http.port=":80" &
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 pid=`ps aux | grep api.finatra | awk '{print $2}'`
-sudo kill -9 $pid
+kill -9 $pid
